aa8f7a09d5c21a1bf92aefaec341cf05add19fad
mrpie95 <michael.pieniazek@gmail.com>
2023-01-09 08:25:26 +0100

    removed google scripts refs

--- PythonScripts/WeightsCalculator.py
+++ PythonScripts/WeightsCalculator.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Exercise:
     def __init__(self, owner, name, weights, bar_mass):
         self._owner = owner
@@ -6,11 +9,13 @@ class Exercise:
         self._weights = weights
 
     def current_mass(self):
-        total_mass = sum(weight.mass * weight.in_use for weight in self._weights)
+        total_mass = sum(
+            weight.mass * weight.in_use for weight in self._weights)
         return total_mass + self._bar_mass
 
     def max_mass(self):
-        total_mass = sum(weight.mass * weight.count for weight in self._weights)
+        total_mass = sum(
+            weight.mass * weight.count for weight in self._weights)
         return total_mass + self._bar_mass
 
     def update_available_weights(self, available_weights):
@@ -30,9 +35,10 @@ class Weight:
         self.count = count
         self.in_use = in_use
 
-def on_open():
-    ui = SpreadsheetApp.get_ui()
-    ui.create_menu('Custom Menu').add_item('Save Data', 'save_data').add_to_ui()
+# def on_open():
+#     ui = SpreadsheetApp.get_ui()
+#     ui.create_menu('Custom Menu').add_item('Save Data', 'save_data').add_to_ui()
+
 
 def intialise_exercises():
     available_weights = [
@@ -48,16 +54,19 @@ def intialise_exercises():
     # exercises.push(new Exercise('Michael','Bench Press', available_weights, 7))
     # exercises.push(new Exercise('Michael','Shoulder Press', available_weights, 7))
 
-    sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
+    # sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
     offset = 29
 
     for exercise in exercises:
         sheet.get_range(f"B{offset}").set_value(json.dumps(exercise))
         offset += 1
 
-#load all exercise and update their weight sets to new weights
+# load all exercise and update their weight sets to new weights
+
+
 def update_available_weights():
     pass
+
 
 def set_exercises():
     available_weights = [
@@ -70,19 +79,19 @@ def set_exercises():
 
     bench_press = Exercise('Kyra', 'Bench Press', available_weights, 7)
 
-    sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
+    # sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
 
-    sheet.get_range('B29').set_value(json.dumps(bench_press))
+    # sheet.get_range('B29').set_value(json.dumps(bench_press))
 
-import json
 
 def load_exercises():
-    sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
-    my_object = json.loads(sheet.get_range('B29').get_value())
+    # sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
+    # my_object = json.loads(sheet.get_range('B29').get_value())
     weights = []
 
     for weight in my_object['_weights']:
-        weights.append(Weight(weight['_mass'], weight['_count'], weight['_inUse']))
+        weights.append(
+            Weight(weight['_mass'], weight['_count'], weight['_inUse']))
 
     exercise = Exercise(
         my_object['_owner'],
@@ -95,12 +104,12 @@ def load_exercises():
     # SpreadsheetApp.get_ui().alert(exercise.current_mass())
     # SpreadsheetApp.get_ui().alert(json.dumps(exercise, indent=2))
 
-    SpreadsheetApp.get_ui().alert(exercise.current_mass())
+    # SpreadsheetApp.get_ui().alert(exercise.current_mass())
 
     exercise.increment_by_smallest_weight()
 
-    SpreadsheetApp.get_ui().alert(exercise.current_mass())
+    # SpreadsheetApp.get_ui().alert(exercise.current_mass())
 
-    sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
+    # sheet = SpreadsheetApp.get_active_spreadsheet().get_sheet_by_name('Ky Data')
 
-    sheet.get_range('B29').set_value(json.dumps(exercise, indent=2))
+    # sheet.get_range('B29').set_value(json.dumps(exercise, indent=2))
